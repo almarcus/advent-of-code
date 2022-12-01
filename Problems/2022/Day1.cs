a6a24dc6f3acc438ba4580a5f96ec010cae58ef3
Aaron Marcus <aaron.lee.marcus@gmail.com>
2022-12-01 00:22:29 -0500

    2022 Day 1 (#13)

---
+++ Problems/2022/Day1.cs
@@ -0,0 +1,31 @@
+namespace AOC2022;
+
+public class Day1
+{
+    List<Elf> elves = new();
+
+    public Day1(string input)
+    {
+        foreach (var foodList in input.Split("\n\n"))
+        {
+            elves.Add(new Elf(foodList));
+        }
+    }
+
+    public int Solve(int numberOfElvesToCheck)
+    {
+        return elves.OrderByDescending(x => x.TotalCalories).Take(numberOfElvesToCheck).Sum(x => x.TotalCalories);
+    }
+}
+
+public class Elf
+{
+    List<int> foodCalories = new();
+
+    public int TotalCalories => foodCalories.Sum();
+
+    public Elf(string foodCarried)
+    {
+        foodCalories = foodCarried.Split('\n', StringSplitOptions.RemoveEmptyEntries).Select(x => int.Parse(x)).ToList();
+    }
+}
--- UnitTests/AoC2022Tests.cs
+++ UnitTests/AoC2022Tests.cs
@@ -1,6 +1,29 @@
 namespace UnitTests;
+using AOC2022;
+using Utilities;
 
 public class AoC2022Tests
 {
+    [Fact]
+    public void Day1aExample()
+    {
+        var input = Utility.ReadFile(@"2022/Inputs/Day1aExample.txt");
+
+        var problem = new Day1(input);
+
+        Assert.Equal(24000, problem.Solve(1));
+    }
+
+    [Fact]
+    public void Day1Solution()
+    {
+        var input = Utility.ReadFile(@"2022/Inputs/Day1.txt");
+
+        var problem = new Day1(input);
+
+        Assert.Equal(71780, problem.Solve(1));
+        Assert.Equal(212489, problem.Solve(3));
+    }
+
 
 }
