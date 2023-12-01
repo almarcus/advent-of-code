72a3c931b90417bfe96e3c98baafe76e010bceaa
Aaron Marcus <aaron.lee.marcus@gmail.com>
2023-12-01 15:33:09 -0500

    Day 1

--- UnitTests/AoC2019Tests.cs
+++ UnitTests/AoC2019Tests.cs
@@ -14,15 +14,32 @@ public class AoC2019Tests
     }
 
     [Theory]
-    [InlineData(@"2023/Inputs/Day1Example1.txt", false, 142)] // Part 1
-    [InlineData(@"2023/Inputs/Day1.txt", false, 55621)] // Part 1
-    [InlineData(@"2023/Inputs/Day1Example2.txt", true, 281)] // Part 2
-    [InlineData(@"2023/Inputs/Day1.txt", true, 53592)] // Part 2
-    public void Day1Solutions(string filename, bool translateSpelledOutNumbers, int expectedResult)
+    [InlineData(12, 2)]
+    [InlineData(14, 2)]
+    [InlineData(1969, 654)]
+    [InlineData(100756, 33583)]
+    public void FuelCalculationByMassTests(int mass, int expectedFuel)
+    {
+        Assert.Equal(expectedFuel, Day1.CalculateFuel(mass, false));
+    }
+
+    [Theory]
+    [InlineData(12, 2)]
+    [InlineData(1969, 966)]
+    [InlineData(100756, 50346)]
+    public void FuelCalculationByMassAccountForFuelTests(int mass, int expectedFuel)
+    {
+        Assert.Equal(expectedFuel, Day1.CalculateFuel(mass, true));
+    }
+
+    [Theory]
+    [InlineData(@"2019/Inputs/Day1.txt", false, 3426455)] // Part 1
+    [InlineData(@"2019/Inputs/Day1.txt", true, 5136807)] // Part 1
+    public void Day1Solutions(string filename, bool accountForFuel, int expectedResult)
     {
         var input = Utility.ReadFile(filename);
+        var requiredFuel = Day1.Solve(input, accountForFuel);
 
-        var problem = new Day1(input, translateSpelledOutNumbers);
-        Assert.Equal(expectedResult, problem.Solve());
+        Assert.Equal(expectedResult, requiredFuel);
     }
 }
